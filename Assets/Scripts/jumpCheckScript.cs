babdb277d1609ba4b4e5a8c4717225c8324cbb81
Z3SPY <92520803+Z3SPY@users.noreply.github.com>
2022-11-16 15:51:55 +0800

    Added Basic Game Movement
    - Added Walking Horizontally
    - Added Jumping

---
+++ Assets/Scripts/jumpCheckScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpCheckScript : MonoBehaviour
+{
+    
+    public playerScript player;
+
+    void Start() {
+        player = GameObject.Find("Player").GetComponent<playerScript>();
+    }
+ 
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Floor")) player.changeJmpState(true);
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        player.changeJmpState(false);
+    }
+
+
+    
+}
--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -6,8 +6,21 @@ public class playerScript : MonoBehaviour
 {
 
     [SerializeField] float x, y;
-    [SerializeField] float _speed = 2f;
-    [SerializeField] bool _canJump = true;
+    [SerializeField] float _speed = 10f;
+    [SerializeField] bool _canJump = false;
+
+    [SerializeField] Rigidbody2D RB2D;
+    [SerializeField] jumpCheckScript jmpChkScript;
+
+    [SerializeField] int jmpNum = 1;
+
+
+    void Awake() {
+
+        if (RB2D == null) RB2D = this.GetComponent<Rigidbody2D>();
+        if (jmpChkScript == null) jmpChkScript = GameObject.Find("bottomCheck").GetComponent<jumpCheckScript>(); 
+        
+    }
 
     void Start()
     {
@@ -16,6 +29,42 @@ public class playerScript : MonoBehaviour
 
     void Update()
     {
+        //Movement
         x = Input.GetAxisRaw("Horizontal");
+
+        if (x != 0) 
+            transform.Translate(new Vector3(x, 0, 0) * Time.deltaTime * _speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _canJump == true) {
+            Jump();
+
+        }
+
+        if (_canJump == false) jmpChkScript.enabled = true;
+        else jmpChkScript.enabled = false;
+
+        
+
     }
+
+    //When Messing With Physics
+    void FixedUpdate() {
+        
+    }
+
+    public void changeJmpState(bool state) {
+
+        _canJump = state;
+        
+
+        Debug.Log("Floored"); 
+
+    }
+
+    void Jump() {
+        Debug.Log("Jump");
+        _canJump = false;  
+        RB2D.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+    }
+
 }
