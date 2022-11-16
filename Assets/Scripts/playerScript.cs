c6ce4926a125ef581eb89b6eb4d2ce2868bc9e5a
Z3SPY <92520803+Z3SPY@users.noreply.github.com>
2022-11-16 12:59:57 +0800

    First Commit
    -Initializing Basic Unity Attrb

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+
+    [SerializeField] float x, y;
+    [SerializeField] float _speed = 2f;
+    [SerializeField] bool _canJump = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+    }
+}
